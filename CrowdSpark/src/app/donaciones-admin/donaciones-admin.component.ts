f462e81a0b8239292544f60f701e6c8d52bff01e
BAMDH <99924195+BAMDH@users.noreply.github.com>
2024-11-11 15:37:54 -0600

    proyecto

--- CrowdSpark/src/app/donaciones-admin/donaciones-admin.component.ts
+++ CrowdSpark/src/app/donaciones-admin/donaciones-admin.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { FirestoreService } from '../firestore.service';
 @Component({
+  standalone:true,
   selector: 'app-donaciones-admin',
+  
+  imports: [CommonModule],
   templateUrl: './donaciones-admin.component.html',
   styleUrls: ['./donaciones-admin.component.css']
 })
@@ -13,12 +17,30 @@ export class DonacionesAdminComponent implements OnInit {
     { projectName: 'Proyecto B', amount: 30, date: '2024-10-15' },
     { projectName: 'Proyecto C', amount: 20, date: '2024-09-25' }
   ];
+  correoUsuario: string | null = null;
+  donaciones: any[] = [];  // Variable para almacenar las donaciones
 
-  constructor(private router: Router) {}
+  constructor(
+    private firestoreService: FirestoreService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-    console.log('Fetching donation history...');
+  ngOnInit() {
+    this.cargarDonaciones();
   }
+
+  // Método para cargar las donaciones desde Firebase
+  cargarDonaciones() {
+    this.firestoreService.getCollectionData('Donacion').subscribe((data: any[]) => {
+      console.log(data);  // Imprimir los datos en consola
+      this.donaciones = data;
+    });
+  }
+  
+  cambiarPantalla() {
+    this.router.navigate(['/pantalla-principal']);
+  }
+ 
 
   goBack() {
     this.router.navigate(['/main-admin']);
